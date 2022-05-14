b619a91ecd213c78179f97401f17273582ffe77c
joao franca <vitorzerokiller@gmail.com>
2022-05-13 21:10:11 -0300

    atualizado

---
+++ src/server.js
@@ -0,0 +1,14 @@
+require ('dotenv').config({path:'variaveis.env'});
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser')
+
+const router = require('./routes');
+
+const server = express();
+server.use(cors());
+server.use(bodyParser.urlencoded({extended: false}));
+
+server.listen(process.env.port,()=>{
+    console.log(`Servidor rodando em http://localhost:${process.env.port}`)
+})
